75b1507c7ce3009d8c870c3b93d38d708d20d04b
Raviraj Sahu <ravirajsahu312@gmail.com>
2026-06-22 11:56:47 +0530

    first commit

---
+++ day_1/main.go
@@ -0,0 +1,10 @@
+package main // A package is a way to group func. It made of the all the files in same directory..
+
+import "fmt"// It is bacically use for formatting the text as well as printing in console. It comes while installing the go lang on system..
+
+
+func main() {  //It is main func, executes by default. when you run main package...
+    fmt.Println("Hello, World!")//Here by using fmt, you can print the output on your console...
+}
+
+// go run <dir>  (This is use for running you go files..)
